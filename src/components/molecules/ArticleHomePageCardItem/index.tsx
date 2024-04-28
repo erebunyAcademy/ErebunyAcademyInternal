702ee5cb24c8813cf76fc525d4ec900ee9e49cd8
Asya Harutyunyan <asya.harutyunyan010@gmail.com>
2024-04-28 13:21:12 +0400

    remove usused components

--- src/components/molecules/ArticleHomePageCardItem/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-type ArticleHomePageCardItemProps = {};
-
-const ArticleHomePageCardItem: FC<ArticleHomePageCardItemProps> = () => {
-  return <div>ArticleHomePageCardItem</div>;
-};
-
-export default ArticleHomePageCardItem;
