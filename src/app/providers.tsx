4b8e9af0bc9a4c9936f9d9aaa503ffd949ca6ca0
Narek Boshyan <narek.boshyan@gmail.com>
2024-05-07 00:44:20 +0400

    working on global loading

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,14 @@
 import { ReactNode } from 'react';
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider, extendTheme, LightMode, ThemeConfig } from '@chakra-ui/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientConfig,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
+import { Loading } from '@/components/atoms';
 import { colors, components, space } from '@/utils/constants/chakra';
 
 const theme: ThemeConfig = extendTheme({
@@ -16,6 +22,17 @@ const theme: ThemeConfig = extendTheme({
   space,
 });
 
+export const gqlGlobalOptions: QueryClientConfig = {
+  queryCache: new QueryCache({}),
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+    mutations: {},
+  },
+};
+
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <SessionProvider>
@@ -24,7 +41,10 @@ const Providers = ({ children }: { children: ReactNode }) => {
           theme={theme}
           toastOptions={{ defaultOptions: { position: 'bottom-right', isClosable: true } }}>
           <LightMode>
-            <QueryClientProvider client={new QueryClient()}>{children}</QueryClientProvider>
+            <QueryClientProvider client={new QueryClient(gqlGlobalOptions)}>
+              <Loading />
+              {children}
+            </QueryClientProvider>
           </LightMode>
         </ChakraProvider>
       </CacheProvider>
