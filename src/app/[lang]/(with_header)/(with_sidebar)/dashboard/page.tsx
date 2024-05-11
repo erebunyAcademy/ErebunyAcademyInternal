ad780c00e78c3c22d1002483a17294b16c1b158c
Asya Harutyunyan <asya.harutyunyan010@gmail.com>
2024-05-11 22:21:33 +0400

    dashboard list

--- src/app/[lang]/(with_header)/(with_sidebar)/dashboard/page.tsx
+++ src/app/[lang]/(with_header)/(with_sidebar)/dashboard/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Avatar, Container, Flex, ListItem, Text, UnorderedList } from '@chakra-ui/react';
 import { serverSession } from '@/pages/api/auth/[...nextauth]';
+import { getUserData } from '@/utils/helpers/user';
 
 const Dashboard = async () => {
   const session = await serverSession();
-  console.log({ session });
+  if (!session?.user) {
+    return null;
+  }
+  const userData = getUserData(session.user);
+
   return (
     <Container maxW={{ base: '95%', md: '80%' }} mt="50px">
       <Flex alignItems="center" gap="20px">
@@ -20,117 +25,19 @@ const Dashboard = async () => {
         </Text>
       </Flex>
       <Flex mt="100px">
-        <UnorderedList styleType="none" width="500px">
-          <ListItem display="flex" justifyContent="space-between">
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              Email:
-            </Text>
-            {session?.user.email}
-          </ListItem>
-          <ListItem display="flex" justifyContent="space-between">
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              Address:
-            </Text>
-            {session?.user.address}
-          </ListItem>
-          <ListItem display="flex" justifyContent="space-between">
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              Country:
-            </Text>
-            {session?.user.country}
-          </ListItem>
-          <ListItem display="flex" justifyContent="space-between">
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              State:
-            </Text>
-            {session?.user.state}
-          </ListItem>
-          <ListItem display="flex" justifyContent="space-between">
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              City:
-            </Text>
-            {session?.user.city}
-          </ListItem>
-          <ListItem display="flex" justifyContent="space-between">
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              Student grade:
-            </Text>
-            {session?.user.student?.studentGrade.title}
-          </ListItem>
-          <ListItem>
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              Student grade group:
-            </Text>
-            {session?.user.student?.studentGradeGroup.title}
-          </ListItem>
-          <ListItem>
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              Faculty:
-            </Text>
-            {session?.user.student?.faculty.title}
-          </ListItem>
-          <ListItem>
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              Profession:
-            </Text>
-            {session?.user.teacher?.profession}
-          </ListItem>
-          <ListItem>
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              Workplace:
-            </Text>
-            {session?.user.teacher?.workPlace}
-          </ListItem>
-          <ListItem>
-            <Text
-              as="span"
-              fontSize={{ base: '16px', sm: '24px' }}
-              fontWeight={700}
-              lineHeight="normal">
-              Scientific Activity:
-            </Text>
-            {session?.user.teacher?.scientificActivity}
-          </ListItem>
+        <UnorderedList w="500px">
+          {userData.map(field => (
+            <ListItem key={field.id} display="flex" justifyContent="space-between">
+              <Text
+                as="span"
+                fontSize={{ base: '16px', sm: '24px' }}
+                fontWeight={700}
+                lineHeight="normal">
+                {field.title}:
+              </Text>
+              {field.value}
+            </ListItem>
+          ))}
         </UnorderedList>
       </Flex>
     </Container>
