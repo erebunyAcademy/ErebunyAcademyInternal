7ef9639e229c06ef79f2fd6ec569e7da93694f73
Hayk Kyutneryan <hayk.kyutneryan@gmail.com>
2024-06-12 10:46:05 +0400

    small fix

--- src/hooks/useFinishExam.ts
+++ src/hooks/useFinishExam.ts
@@ -1,14 +1,9 @@
 import { useMutation } from '@tanstack/react-query';
-import { redirect } from 'next/navigation';
 import { ExamService } from '@/api/services/exam.service';
-import { ROUTE_STUDENT_EXAM_LIST } from '@/utils/constants/routes';
 
 const useFinishExam = () => {
   return useMutation<boolean, { message: string }, string>({
     mutationFn: ExamService.finishExam,
-    onSuccess() {
-      redirect(ROUTE_STUDENT_EXAM_LIST);
-    },
   });
 };
 
