37d68110c4264d92c3a8a4af2ab1e58c45d3b516
Narek Boshyan <narek.boshyan@gmail.com>
2024-06-17 02:01:58 +0400

    working on react time

--- src/components/molecules/ExamTimer/index.tsx
+++ src/components/molecules/ExamTimer/index.tsx
@@ -1,9 +1,10 @@
 'use client';
-import React, { FC, memo, useCallback, useEffect, useState } from 'react';
-import { Box, Text, useDisclosure } from '@chakra-ui/react';
+import React, { FC, memo, useCallback } from 'react';
+import { Box, useDisclosure } from '@chakra-ui/react';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import { useRouter } from 'next/navigation';
+import { useTimer } from 'react-timer-hook';
 import useFinishExam from '@/hooks/useFinishExam';
 import { ROUTE_STUDENT_EXAM_LIST } from '@/utils/constants/routes';
 import ExamResultsModal from '../ExamResultsModal';
@@ -24,52 +25,35 @@ const ExamTimer: FC<ExamTimeProps> = ({
   examTranslationId,
 }) => {
   const { mutate: finish } = useFinishExam();
-  const [timeLeft, setTimeLeft] = useState(dayjs.duration(0));
-  const [isFinished, setIsFinished] = useState(false);
   const router = useRouter();
 
   const handleClose = useCallback(() => router.push(ROUTE_STUDENT_EXAM_LIST), [router]);
 
   const { isOpen, onClose, onOpen } = useDisclosure({ onClose: handleClose });
 
-  const calculateTimeLeft = useCallback(() => {
-    const now = dayjs();
-    const endTime = dayjs(startTime).add(durationInMinutes, 'minute');
-    const timeRemaining = endTime.diff(now);
+  const expiryTimestamp = dayjs(startTime).add(durationInMinutes, 'minute').toDate();
 
-    if (timeRemaining > 0) {
-      return dayjs.duration(timeRemaining);
-    } else {
-      return dayjs.duration(0);
-    }
-  }, [durationInMinutes, startTime]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const timeRemaining = calculateTimeLeft();
-      setTimeLeft(timeRemaining);
-
-      if (timeRemaining.asSeconds() <= 0 && !isFinished) {
-        setIsFinished(true);
-        finish({ examId }, { onSuccess: onOpen });
-        clearInterval(interval);
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [calculateTimeLeft, examId, finish, isFinished, onOpen, router]);
-
-  const formatTime = (timeDuration: duration.Duration) => {
-    const minutes = timeDuration.minutes();
-    const seconds = timeDuration.seconds();
-    return `${minutes}m ${seconds}s`;
-  };
+  const { seconds, minutes, hours } = useTimer({
+    expiryTimestamp,
+    onExpire: () => {
+      finish(
+        { examId },
+        {
+          onSuccess() {
+            onOpen();
+          },
+        },
+      );
+    },
+  });
 
   return (
     <Box>
-      <Text as="h2" fontSize={24} textAlign="center">
-        Time Left: {formatTime(timeLeft)}
-      </Text>
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ fontSize: '100px' }}>
+          <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+        </div>
+      </div>
 
       <ExamResultsModal
         isOpen={isOpen}
