57c9750a9accccb508592a5b8fd921dee1526e48
Narek Boshyan <narek.boshyan@gmail.com>
2024-09-03 00:36:29 +0400

    working on debugging

--- src/lib/prisma/error/index.ts
+++ src/lib/prisma/error/index.ts
@@ -7,7 +7,7 @@ export const exceptionHandler = (
   req: NextApiRequest,
   res: NextApiResponse,
 ) => {
-  console.log(error);
+  console.error(error);
   res.status(error instanceof HttpException ? error.statusCode : 500).json({
     message:
       !(error instanceof HttpException) && process.env.NODE_ENV !== 'development'
